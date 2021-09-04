ad504b7fee1b6e19ea7588a217d6669547bac40b
EC2 Default User <ec2-user@ip-172-31-64-38.ec2.internal>
2021-09-04 15:39:15 +0000

    add

---
+++ lesson7-2.rb
@@ -0,0 +1,12 @@
+puts "計算を始めます"
+puts "何回計算を繰り返しますか？"
+times = gets.to_i
+for times in 1..times do
+  puts "#{times}回目の計算"
+  num1 = gets.to_i
+  num2 = gets.to_i
+  puts "num1+num2=#{num1 + num2}"
+  puts "num1-num2=#{num1 - num2}"
+  puts "num1*num2=#{num1 * num2}"
+  puts "num1/num2=#{num1 / num2}"
+end
