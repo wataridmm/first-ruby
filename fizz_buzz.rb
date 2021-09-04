91d1489d8ff6c5afd20f888936812975e5345e4f
EC2 Default User <ec2-user@ip-172-31-64-38.ec2.internal>
2021-09-04 16:53:08 +0000

    add

---
+++ fizz_buzz.rb
@@ -0,0 +1,22 @@
+def fizz_buzz(number)
+  if number % 15 == 0
+  "fizz_buzz"
+  elsif
+    number % 3 == 0
+    "fizz"
+    elsif
+      number % 5 == 0
+      "buzz"
+    else
+      number.to_s
+  end
+end
+
+
+
+puts "数字を入力してください"
+
+input = gets.to_i
+
+puts "結果は..."
+puts fizz_buzz(input)
